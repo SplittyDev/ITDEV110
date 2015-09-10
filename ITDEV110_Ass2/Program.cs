bbd45b2cb47922a8daae706a4c47280c5d804765
Splitty <splittydev@gmail.com>
2015-09-10 17:24:43 +0200

    Initial commit

---
+++ ITDEV110_Ass2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ITDEV110_Ass2
+{
+	class MainClass
+	{
+		public static void Main (string[] args) {
+			var name = "Splitty";
+			Console.WriteLine ("Char\tHex\tOct\tBin");
+			foreach (var c in name)
+				Console.WriteLine ("{0:2}\t0x{1:X2}\t{2:000}h\tb{3}", c, (byte)c, Convert.ToString (c, 8), Convert.ToString (c, 2));
+			Console.Read ();
+		}
+	}
+}
